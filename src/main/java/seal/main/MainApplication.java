1fe2a9126f19fead64da1e3e0fd8d919982e0403
BrianM0330 <BrianM0330@gmail.com>
2021-06-07 20:23:12 -0500

    applicant endpoint

--- src/main/java/seal/main/MainApplication.java
+++ src/main/java/seal/main/MainApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
+@RestController
 public class MainApplication {
+
+	@GetMapping("/applicant")
+	public Applicant createApplicant(@RequestParam(value="name", defaultValue = "test") String name) throws SQLException, ClassNotFoundException {
+		return new Applicant();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
